61922c2fe4d11fe8bc64f519a4e082e1beec1625
nisangupta <nishangupta@icloud.com>
2020-09-01 11:47:03 +0545

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <v-app>
-    <v-main>
-      <calendar />
-    </v-main>
+    <v-main></v-main>
   </v-app>
 </template>
 
 <script>
-import Calendar from "./components/Calendar";
-
 export default {
   name: "App",
 
-  components: {
-    Calendar,
-  },
-
-  data: () => ({
-    //
-  }),
+  components: {},
 };
 </script>
